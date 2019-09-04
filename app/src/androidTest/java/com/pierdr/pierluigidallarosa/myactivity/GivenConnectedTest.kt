3e7836e534340d35a1868540b8c8b4fb35533609
Enrico Gueli <enrico.gueli@gmail.com>
2019-09-04 19:08:02 +0200

    less flakyness

--- app/src/androidTest/java/com/pierdr/pierluigidallarosa/myactivity/GivenConnectedTest.kt
+++ app/src/androidTest/java/com/pierdr/pierluigidallarosa/myactivity/GivenConnectedTest.kt
@@ -1,6 +1,7 @@
 package com.pierdr.pierluigidallarosa.myactivity
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
@@ -8,6 +9,7 @@ import com.pierdr.tramontana.ui.TramontanaActivity
 import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -19,6 +21,7 @@ class GivenConnectedTest {
     @get:Rule
     val activityTestRule = ActivityTestRule(TramontanaActivity::class.java, true, true)
 
+    private lateinit var sketchVisibilityIdlingResource: ViewVisibilityIdlingResource
     private val endpoint: WebSocketEndpoint = mockk(relaxed = true)
     private val client = MockableClient(endpoint)
 
@@ -26,6 +29,14 @@ class GivenConnectedTest {
     fun setUp() {
         client.connect()
         verify(timeout = 1000) { endpoint.onOpen(any()) }
+
+        sketchVisibilityIdlingResource = ViewVisibilityIdlingResource(activityTestRule.activity.findViewById(R.id.sketch_container))
+        IdlingRegistry.getInstance().register(sketchVisibilityIdlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(sketchVisibilityIdlingResource)
     }
 
     @Test
---
+++ app/src/androidTest/java/com/pierdr/pierluigidallarosa/myactivity/ViewVisibilityIdlingResource.kt
@@ -0,0 +1,33 @@
+package com.pierdr.pierluigidallarosa.myactivity
+
+import android.view.View
+
+import androidx.test.espresso.IdlingResource
+
+class ViewVisibilityIdlingResource(
+        private val view: View,
+        private val expectedVisibility: Int = View.VISIBLE
+) : IdlingResource {
+
+    private var idle: Boolean = false
+    private var resourceCallback: IdlingResource.ResourceCallback? = null
+
+    override fun getName(): String {
+        return ViewVisibilityIdlingResource::class.java.simpleName
+    }
+
+    override fun isIdleNow(): Boolean {
+        idle = idle || view.visibility == expectedVisibility
+
+        if (idle) {
+            resourceCallback?.onTransitionToIdle()
+        }
+
+        return idle
+    }
+
+    override fun registerIdleTransitionCallback(resourceCallback: IdlingResource.ResourceCallback) {
+        this.resourceCallback = resourceCallback
+    }
+
+}
